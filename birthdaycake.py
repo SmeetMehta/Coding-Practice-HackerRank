fc2935b4cb9c3e76897add6b2dcc879e85c6af8c
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-06-19 11:24:21 +0530

    Create birthdaycake.py
    count the occurrences of the maximum element in the list

---
+++ birthdaycake.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+
+def birthdayCakeCandles(candles):
+    x=max(candles)
+    y=candles.count(x)
+    return y
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
